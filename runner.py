b9cd4a27570f9fd97f04c8e1dbb5692a36e650a4
Kevin Luvian <kluvian001@e.ntu.edu.sg>
2017-09-05 01:19:51 +0800

    add info to runner

--- runner.py
+++ runner.py
@@ -5,9 +5,11 @@ import time
 def gen_argv(s):
     sys.argv = s.split()
 
-
+cnt = 0
 if __name__ == '__main__':
     gen_argv('scrapy crawl foursquare')
     while True:
+        cnt += 1
+        print('Running', cnt);
         execute()
         time.sleep(3600)
