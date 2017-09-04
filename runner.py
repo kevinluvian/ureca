6b20a4c51d1f71b5b3579f245030b8179dbc590a
Kevin Luvian <kluvian001@e.ntu.edu.sg>
2017-09-05 01:54:19 +0800

    fix scheduler

--- runner.py
+++ runner.py
@@ -1,15 +1,10 @@
-import sys
-from scrapy.cmdline import execute
+from subprocess import call
 import time
 
-def gen_argv(s):
-    sys.argv = s.split()
-
 cnt = 0
-if __name__ == '__main__':
-    gen_argv('scrapy crawl foursquare')
-    while True:
-        cnt += 1
-        print('Running', cnt);
-        execute()
-        time.sleep(3600)
+while True:
+    cnt += 1
+    print('Running', cnt)
+    call(["scrapy", "crawl", "foursquare"])
+    print('finished')
+    time.sleep(3600)
