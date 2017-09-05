ea7ca176d5fb5953a1d5974d62deec9764c4a3de
Kevin Luvian <kluvian001@e.ntu.edu.sg>
2017-09-05 12:24:03 +0800

    add elapsed time

--- runner.py
+++ runner.py
@@ -5,6 +5,8 @@ cnt = 0
 while True:
     cnt += 1
     print('Running', cnt)
+    start_time = time.time()
     call(["scrapy", "crawl", "foursquare"])
-    print('finished')
+    elapsed_time = time.time() - start_time
+    print('finished', elapsed_time)
     time.sleep(3600)
